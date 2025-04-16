9c7d1cbcf88d2854bdf6185e3a069b36ad0b5386
CompetentHeisenberg <competentheisenberg@gmail.com>
2025-04-16 13:22:34 +0300

    Авторизація

--- app/src/main/java/com/example/yourhealth/LoginActivity.kt
+++ app/src/main/java/com/example/yourhealth/LoginActivity.kt
@@ -5,19 +5,61 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
 
-        val button = findViewById<Button>(R.id.regButton)
-        button.setOnClickListener {
+        auth = FirebaseAuth.getInstance()
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val registerButton = findViewById<Button>(R.id.regButton)
+        val emailInput = findViewById<EditText>(R.id.emailInput)
+        val passwordInput = findViewById<EditText>(R.id.passInput)
+
+        loginButton.setOnClickListener {
+            val email = emailInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                signInUser(email, password)
+            } else {
+                Toast.makeText(this, "Заповніть всі поля", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        registerButton.setOnClickListener {
             val intent = Intent(this, RegActivity::class.java)
             startActivity(intent)
         }
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         supportActionBar?.hide()
+    }
+
+    private fun signInUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        this,
+                        "Успішний вход! Добро пожалувати!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Ошибка входу: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 }
